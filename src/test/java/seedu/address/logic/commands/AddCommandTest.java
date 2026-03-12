2cad171739dcf7cf286982c02aa8fbeee4349b24
archonward <arthurteng@outlook.com>
2026-03-12 22:22:22 +0800

    Fixed gradle unable to compile due to errors in test files

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -22,6 +22,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.milestone.StudentId;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
@@ -146,6 +147,12 @@ public class AddCommandTest {
         @Override
         public void setPerson(Person target, Person editedPerson) {
             throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public StudentId getNextStudentId() {
+            // Provide a stub implementation as appropriate for your test
+            return null;
         }
 
         @Override
