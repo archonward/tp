4adffc107de9713cd9c0e6200352fc941fed16da
Ryan Long <52093793+sehjinryan@users.noreply.github.com>
2026-03-25 15:43:21 +0800

    Remove unused import from ParserUtil

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
